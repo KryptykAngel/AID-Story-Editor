0b9091b9e217fe133365cbb1be76aeb4360ffe63
KryptykAngel <101753410+KryptykAngel@users.noreply.github.com>
2025-05-12 19:25:06 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// Files to cache for offline access
+const CACHE_NAME = "aid-story-editor-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install event - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event - serve cached assets when available
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
